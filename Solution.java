eb57f0b71dd3a535cf5dd6d650a09ad3566e5a3b
yurrim <yurimkang20@gmail.com>
2022-05-22 21:47:58 +0900

    정답 제출용

---
+++ Solution.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+class Solution
+{
+	public static void main(String args[]) throws Exception
+	{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+
+		int T = Integer.parseInt(br.readLine());
+
+		for (int i = 0; i < T; i++) {
+			sb.append("#");
+		}
+
+		System.out.println(sb);
+		br.close();
+	}
+}
